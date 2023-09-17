a72fbeefdde8768ca009d44b1e391bedc54091ba
jeh15 <jeh15@fsu.edu>
2023-09-17 05:33:52 -0400

    Added Eigen dependency to map numpy.ndarray to Eigen::MatrixXd

--- lowlevelapi/src/digit_api_test.py
+++ lowlevelapi/src/digit_api_test.py
@@ -1,10 +1,22 @@
 import digit_api
+import numpy as np
 
 
 def main(argv=None):
     publisher_address = "127.0.0.1"
     digit_api.initialize_communication(publisher_address, 25501, 25500)
     print(f"Connection status: {digit_api.check_connection()}")
+
+    motor_array = np.hstack(
+        [
+            1 * np.ones((20, 1), dtype=np.float64),
+            2 * np.ones((20, 1), dtype=np.float64),
+            3 * np.ones((20, 1), dtype=np.float64),
+        ],
+    )
+    print(motor_array.shape)
+    # motor_array = np.zeros((20, 3), dtype=np.float64)
+    digit_api.send_command(motor_array, 0, False)
 
 
 if __name__ == "__main__":
--- lowlevelapi/src/digitapi_py.cc
+++ lowlevelapi/src/digitapi_py.cc
@@ -2,30 +2,49 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/eigen.h>
+
+#include <iostream>
 
 namespace py = pybind11;
 
+// connection function: Keep sending a message until a response is received.
+
+
 // Wrapper for send command:
+// void wrapper_llapi_send_command(
+//     py::array_t<double, py::array::c_style | py::array::forcecast> array,
+//     int32_t fallback_opmode,
+//     bool apply_command
+// ) {
+//     // Map Numpy array to llapi_motor_t struct:
+//     llapi_command_t command = {0};
+//     for(int i = 0; i < NUM_MOTORS; i++) {
+//         command.motors[i].torque = array.at(i, 0);
+//         command.motors[i].velocity = array.at(i, 1);
+//         command.motors[i].damping = array.at(i, 2);
+
+//         std::cout << "Torque: " << command.motors[i].torque << std::endl;
+//     }
+// }
+
 void wrapper_llapi_send_command(
-    py::array_t<double, py::array::c_style | py::array::forcecast> array,
+    const Eigen::Ref<const Eigen::MatrixXd>& array,
     int32_t fallback_opmode,
     bool apply_command
 ) {
     // Map Numpy array to llapi_motor_t struct:
+    llapi_command_t command = {0};
+    for(int i = 0; i < NUM_MOTORS; i++) {
+        command.motors[i].torque = array(i, 0);
+        command.motors[i].velocity = array(i, 1);
+        command.motors[i].damping = array(i, 2);
+    }
+    command.fallback_opmode = fallback_opmode;
+    command.apply_command = apply_command;
 
-    // Data structure to map:
-    // Motor:
-    // typedef struct __attribute__((packed)) {
-    //   double torque;
-    //   double velocity;
-    //   double damping;
-    // } llapi_motor_t;
-    // Command:
-    // typedef struct __attribute__((packed)) {
-    //   llapi_motor_t motors[NUM_MOTORS];
-    //   int32_t fallback_opmode;
-    //   bool apply_command;
-    // } llapi_command_t;
+    // Send command:
+    llapi_send_command(&command);
 }
 
 PYBIND11_MODULE(digit_api, m) {
@@ -46,8 +65,12 @@ PYBIND11_MODULE(digit_api, m) {
         "get_states", &llapi_get_observation, "Returns a copy of Digit's current states."
     );
 
+    // m.def(
+    //     "send_command", &llapi_send_command, "Sends a command to Digit."
+    // );
+
     m.def(
-        "send_command", &llapi_send_command, "Sends a command to Digit."
+        "send_command", &wrapper_llapi_send_command, "Sends a command to Digit."
     );
 
     // Limit Functions:
@@ -55,4 +78,16 @@ PYBIND11_MODULE(digit_api, m) {
         "get_limits", &llapi_get_limits, "Returns a copy of Digit's command limits."
     );
 
+    // py::class_<llapi_motor_t>(m, "Motor")
+    //     .def(py::init<>())
+    //     .def_readwrite("torque", &llapi_motor_t::torque)
+    //     .def_readwrite("velocity", &llapi_motor_t::velocity)
+    //     .def_readwrite("damping", &llapi_motor_t::damping);
+
+    // py::class_<llapi_command_t>(m, "Command")
+    //     .def(py::init<>())
+    //     .def_readwrite("motors", &llapi_command_t::motors)
+    //     .def_readwrite("fallback_opmode", &llapi_command_t::fallback_opmode)
+    //     .def_readwrite("apply_command", &llapi_command_t::apply_command);
+    
 }
